a7b851f46eeb957ca38c9b8f15f6dfeb8457b36e
hi-hi-ray <dutra.raysa@gmail.com>
2021-03-01 11:58:09 -0300

    feat: add errors messages

---
+++ api/errors/error_messages.go
@@ -0,0 +1,4 @@
+package errors
+
+const MissingConfigVariable = "The following variables in config toml file is missing: "
+const ProblemAtReadingFile = "The following problem occurred when trying to read the toml file: "

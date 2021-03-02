d4266d36d75ffa556c437ef00a8fcdc2ebe01b58
hi-hi-ray <dutra.raysa@gmail.com>
2021-03-02 00:26:19 -0300

    feat: add swapi error messages

--- api/errors/error_messages.go
+++ api/errors/error_messages.go
@@ -2,3 +2,8 @@ package errors
 
 const MissingConfigVariable = "The following variables in config toml file is missing: "
 const ProblemAtReadingFile = "The following problem occurred when trying to read the toml file: "
+const SwapiConsumerError = "The following error occurred when trying to request the api: "
+const ReadBodyError = "The following error occurred when trying to read the body: "
+const MarshallBodyError = "The following error occurred when trying to parse the component: "
+const SwapiInternalError = "An unmapped error happened."
+const SwapiNotFoundError = "The planet was not found."

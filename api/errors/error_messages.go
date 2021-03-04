d30dd6667ef09fc46b3f23c5e99d45d9fc160ded
hi-hi-ray <dutra.raysa@gmail.com>
2021-03-04 19:38:07 -0300

    feat: add more error messages

--- api/errors/error_messages.go
+++ api/errors/error_messages.go
@@ -7,3 +7,6 @@ const ReadBodyError = "The following error occurred when trying to read the body
 const MarshallBodyError = "The following error occurred when trying to parse the component: "
 const SwapiInternalError = "An unmapped error happened."
 const SwapiNotFoundError = "The planet was not found."
+const PlanetExist = "This planet already exits, check him using his id. You can use the method GET to find him."
+const PlanetMandatoryFields = "Check the fields name, terrain and climate they are mandatory"
+const PlanetDoesNotExist = "This planet doesn't exist, check his id. You can use the method GET to find him."
